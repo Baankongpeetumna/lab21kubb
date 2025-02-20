b3922bc651255267c9fe47e4b8d8f06310048fc0
Benyaporn <Baibua41430@gmail.com>
2025-02-20 23:01:48 +0700

    lab21cp1

--- average.cpp
+++ average.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
 
+int main(int argc, char* argv[]) {
+    vector<double> numbers;
+    
+    if (argc < 2) {
+        cout << "Please input numbers to find average.\n";
+        return 0;
+    }
+    
+    for (int i = 1; i < argc; i++) {
+        numbers.push_back(atof(argv[i]));
+    }
+    
+    double sum = 0;
+    for (double i = 0; i < numbers.size(); i++) {
+        sum += numbers[i];
+    }
+    
+    double average = sum / numbers.size();
+    
+    cout << "---------------------------------\n";
+    cout << "Average of " << numbers.size() << " numbers = " << average << "\n";
+    cout << "---------------------------------\n";
+    
+    return 0;
+}
